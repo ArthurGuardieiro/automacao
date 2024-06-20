808d5e739593b4d2fe8d71b311cf2870cdb4a66d
Arthur Guardieiro <arthur.aaguardieiro@icloud.com>
2024-06-20 14:40:53 -0300

    utilizando dataTable

--- didatikos/login/features/login.feature
+++ didatikos/login/features/login.feature
@@ -5,11 +5,25 @@ Funcionalidade: login
     Quero fazer login no sistema
     Para que eu possa utilizar o software
 
-    
+    @login
     Cenario: Fazer login
 
         Dado que acesso a página de login
-        Quando submeto o meu login correto
+        Quando submeto o seguinte formulário de cadastro:
+        |email              |senha         |
+        |admin@ditatikos.com|didatikos@1010|
         Então sou redirecionado para o Dashboard
 
     
+    Cenario: Fazer login sem o email
+
+        Dado que acesso a página de login
+        Quando submeto o meu login sem o email
+        Então vejo a mensagem de alerta: "O campo e-mail é obrigatório."
+
+    
+    Cenario: Fazer login sem senha
+
+        Dado que acesso a página de login
+        Quando submeto o meu login sem a senha
+        Então vejo a mensagem de alerta: "O campo senha é obrigatório."
--- didatikos/login/features/step_definitions/login_steps.rb
+++ didatikos/login/features/step_definitions/login_steps.rb
@@ -2,9 +2,12 @@ Dado('que acesso a página de login') do
     visit "https://didatikos-hmg.didatikos.com.br/ava/login"
 end
   
-Quando('submeto o meu login correto') do
-    find('#email').set "admin@ditatikos.com"
-    find('#password').set "didatikos@1010"
+Quando('submeto o seguinte formulário de cadastro:') do |table|
+    
+    user = table.hashes.first
+
+    find("#email").set user[:email]
+    find("#password").set user[:senha]
 
     click_button "Login"
 
@@ -12,4 +15,22 @@ end
   
 Então('sou redirecionado para o Dashboard') do
     expect(page).to have_css ".app-body"
+end
+
+Quando('submeto o meu login sem o email') do
+    find('#password').set "pwd123"
+
+    click_button "Login"
+end
+  
+Então('vejo a mensagem de alerta: {string}') do |expect_alert|
+    alert = find(:xpath, '//*[@id="login-form"]/div[2]/div[1]/ul/li')
+    expect(alert.text).to eql expect_alert
+end
+
+
+Quando('submeto o meu login sem a senha') do
+    find('#email').set "admin@ditatikos.com"
+
+    click_button "Login"
 end

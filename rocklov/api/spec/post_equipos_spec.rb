32f1a281909e90b3250fe5325efb66d6fc75dcf1
Arthur Guardieiro <arthur.aaguardieiro@icloud.com>
2024-08-08 17:14:52 -0300

    rocklov

--- rocklov/api/spec/post_equipos_spec.rb
+++
@@ -1,6 +0,0 @@
-require_relative "routes/equipos"
-
-describe "POST/ equipos" do
-  context "novo equipamento" do
-  end
-end
--- rocklov/api/spec/spec_helper.rb
+++ rocklov/api/spec/spec_helper.rb
@@ -1,3 +1,16 @@
+require_relative "routes/signup"
+require_relative "routes/sessions"
+require_relative "routes/equipos"
+
+require_relative "libs/mongo"
+require_relative "helpers"
+
+require "digest/md5"
+
+def to_md5(pass)
+  return Digest::MD5.hexdigest(pass)
+end
+
 RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
@@ -8,4 +21,16 @@ RSpec.configure do |config|
   end
 
   config.shared_context_metadata_behavior = :apply_to_host_groups
+
+  config.before(:suite) do
+    users = [
+      { name: "Roberto Carlos", email: "betao@yahoo.com", password: to_md5("pwd123") },
+      { name: "Penelope", email: "penelope@gmail.com", password: to_md5("pwd123") },
+      { name: "Edward Newgate", email: "ed@gmail.com", password: to_md5("pwd123") },
+      { name: "Joe", email: "joe@gmail.com", password: to_md5("pwd123") },
+    ]
+
+    MongoDB.new.drop_danger
+    MongoDB.new.insert_users(users)
+  end
 end

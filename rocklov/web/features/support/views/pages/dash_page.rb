1bce346751c203f3ffc166fcdfeab16b6cd7b739
Arthur Guardieiro <arthur.aaguardieiro@icloud.com>
2024-08-12 16:01:33 -0300

    rocklov

--- rocklov/web/features/support/views/pages/dash_page.rb
+++ rocklov/web/features/support/views/pages/dash_page.rb
@@ -2,10 +2,35 @@ class DashPage
   include Capybara::DSL
 
   def on_dash?
-    return page.have_css?(".dashboard")
+    return page.has_css?(".dashboard")
   end
 
   def equipot_list
     return find(".equipo-list")
+  end
+
+  def has_no_equipo?(name)
+    return page.has_no_css?(".equipo-list li", text: name)
+  end
+
+  def request_removal(name)
+    equipo = find(".equipo-list li", text: name)
+    equipo.find(".delete-icon").click
+  end
+
+  def confirm_removal
+    click_on "Sim"
+  end
+
+  def cancel_removal
+    click_on "Não"
+  end
+
+  def order
+    return find(".notifications p")
+  end
+
+  def order_actions(name)
+    return page.has_css?(".notifications button", text: name)
   end
 end
